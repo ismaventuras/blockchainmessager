42d3c356e347a6a916fd4d4dbe46ef4b9710d9da
HispanicSquirtle <ismaelbautista6@gmail.com>
2022-02-22 00:37:19 +0100

    updated icons

--- src/utils/web3Utils.js
+++ src/utils/web3Utils.js
@@ -15,3 +15,9 @@ export const addCommas = num => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, 
 
 // pass an address and return a shortened version
 export const shortenAddress = address => `${address.substring(0,6)}...${address.substring(address.length-4)}`
+
+const isContract = async (address, provider) => {
+    let code = await provider.getCode(address,"latest")
+    let _isContract = code === "0x" ? false : true
+    return _isContract
+}
